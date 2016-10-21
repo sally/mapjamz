09e1a4703cfdb4e3f25993ff1c558c0fa0faeb34
Young Ju Park <parkyngj@gmail.com>
2016-10-21 07:37:56 -0700

    Change hardcoded track summary to be consistent upon location search

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,23 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
 
+  before_save :assign_random_compliment
+
+  def assign_random_compliment
+    write_attribute(:compliment, [
+      "When my life flashes before my eyes, I hope it's mostly you.",
+      "You're the bee's knees, the cat's pyjamas, the pheasant's bowtie.",
+      "If we were marooned on an island, I'd be very sad when I had to eat you.",
+      "You are more adorable than a baby panda riding on a sneezing piglet.",
+      "If you were a dinosaur, you'd be a Legendasaurus Rex.",
+      "Is it a bird? Is it a plane? No. It's you, you massive legend.",
+      "8 out of 10 cats prefer you.",
+      "You're the first person I'd look for after an apocalyptic event.",
+      "Things I wish I could be: Ninja. Astronaut. You.",
+      "You're better than free wi-fi."
+    ].sample)
+  end
+
   def password
     @password ||= BCrypt::Password.new(password_hash)
   end
