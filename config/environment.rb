f2b487fad6e89e79bc597851c6581eb04cc0af85
Zino <rhu5@u.rochester.edu>
2016-12-14 20:15:11 -0600

    Normalize country input

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,8 @@ require 'erb'
 require 'faker'
 require 'pry'
 
+require 'normalize_country'
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
