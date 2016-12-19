effdc822aad0249c9496242cf65bde337448d1c4
Young Ju Park <parkyngj@gmail.com>
2016-12-19 10:04:46 -0800

    adds iso_name option to normalize country

--- app/services/lastfm_service.rb
+++ app/services/lastfm_service.rb
@@ -3,28 +3,8 @@ module LastFmService
 
   KEY = ENV['LAST_FM_API_KEY']
 
-  ## OBSOLETE CODE FOR GETTING SONG INFO FROM LASTFM
-  # def self.song_info(artist,song)
-  #   p url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=9fc86c40ce0f4704f7e83f97c491ddfa&artist=#{artist}&track=#{song}&format=json"
-  #   p uri = Addressable::URI.parse(url).normalize.to_s.gsub(/(\%)/, " ")
-
-  #   pry
-
-  #   response = HTTParty.get(uri)
-
-  #   parsed_response = response.parsed_response["track"]
-  # end
-
-  # def self.get_song_description(artist,song)
-  #   if song_info(artist,song)["wiki"]
-  #     song_info(artist,song)["wiki"]["summary"].gsub("\n", "").gsub(/(\s*\<.*\/.*\>.?)/, '')
-  #   else
-  #     Faker::Lorem.paragraph
-  #   end
-  # end
-
   def self.top_tracks(country)
-    country = NormalizeCountry(country)
+    country = NormalizeCountry(country, :to => :iso_name)
 
     url = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=#{country}&limit=10&api_key=#{KEY}"
 
