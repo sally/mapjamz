4a18f7f1f1102a18f5e942090fce672e243d03fd
Zino <rhu5@u.rochester.edu>
2016-12-31 20:32:25 -0600

    Update youtube_service to also grab video image_url
    * Since Melon doesn’t provide artist image, we can grab a video image
    from Youtube instead for South Korean listings.
    * Instead of checking for ascii_only in the url, we can use URI::encode
    to ensure urls are valid.

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -2,18 +2,27 @@ module YoutubeService
   include HTTParty
 
   KEY = ENV['GOOGLE_API_KEY']
-
-  def self.get_first_video_id(query)
+  
+  # Relevant Youtube api response example: 
+  # {"items"=>
+  #   [{"id"=>{"kind"=>"youtube#video", "videoId"=>"z1gn-CRc0qQ"},
+  #     "snippet"=>
+  #      { "thumbnails"=>
+  #        {"default"=>{"url"=>"https://i.ytimg.com/vi/z1gn-CRc0qQ/default.jpg", "width"=>120, "height"=>90},
+  #         "medium"=>{"url"=>"https://i.ytimg.com/vi/z1gn-CRc0qQ/mqdefault.jpg", "width"=>320, "height"=>180},
+  #         "high"=>{"url"=>"https://i.ytimg.com/vi/z1gn-CRc0qQ/hqdefault.jpg", "width"=>480, "height"=>360}} 
+  #       }}]}
+  def self.download_track_data(query)
     url = "https://www.googleapis.com/youtube/v3/search?key=#{KEY}&type=video&part=snippet&maxResults=1&q=#{query}".gsub("\u{d8}", "")
+    response = HTTParty.get(URI::encode(url)).parsed_response
 
-    if url.ascii_only?
-      response = HTTParty.get(url)
-
-      parsed_response = response.parsed_response
-
-      parsed_response['items'].first['id']['videoId']
+    if response['items'].present?
+      youtube_id = response['items'].first['id']['videoId']
+      image_url = response['items'].first['snippet']['thumbnails']['default']['url']
+      [image_url, youtube_id]
     else
-      ["ZXVhOPiM4mk", "d9TpRfDdyU0"].sample
+      youtube_id = ["ZXVhOPiM4mk", "d9TpRfDdyU0"].sample
+      [nil, youtube_id]
     end
   end
 end
